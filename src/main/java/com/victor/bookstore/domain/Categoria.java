575f0757cad955da35854e21efffae68e218347d
vtvictor <victorluiz.bsi@gmail.com>
2023-06-15 12:34:37 -0300

    Classes de dominio

---
+++ src/main/java/com/victor/bookstore/domain/Categoria.java
@@ -0,0 +1,76 @@
+package com.victor.bookstore.domain;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.ArrayList;
+
+public class Categoria {
+	private Integer id;
+	private String nome;
+	private String descricao;
+	
+	private List<Livro> livros = new ArrayList<>();
+
+	public Categoria() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Categoria(Integer id, String nome, String descricao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Livro> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(List<Livro> livros) {
+		this.livros = livros;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+		
+	
+}
